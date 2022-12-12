c9e121e95342fe868dd9003715277fa2a9a2f940
Jean-Franco <jean-franco.zarate@sansano.usm.cl>
2022-12-12 15:35:41 -0300

    Added getters and setters, register

--- Proyecto 3/Biblioteca/src/Bib/Libro.java
+++ Proyecto 3/Biblioteca/src/Bib/Libro.java
@@ -1,18 +1,18 @@
 package Bib;
 
 public class Libro {
-	String titulo;
-	String autor;
-	String fecha;
-	int paginas;
-	String editorial;
-	String genero;
-	String isbn;
-	String ubicacion;
-	String estado;
-	String descripcion;
+	private String titulo;
+	private String autor;
+	private String fecha;
+	private int paginas;
+	private String editorial;
+	private String genero;
+	private String isbn;
+	private String ubicacion;
+	private String estado;
+	private String desc;
 	
-	public Libro(String titulo, String autor, String fecha, int paginas, String editorial, String genero, String isbn, String ubicacion, String estado, String descripcion) {
+	public Libro(String titulo, String autor, String fecha, int paginas, String editorial, String genero, String isbn, String ubicacion, String estado, String desc) {
 		this.titulo = titulo;
 		this.autor = autor;
 		this.fecha = fecha;
@@ -21,6 +21,66 @@ public class Libro {
 		this.genero = genero;
 		this.ubicacion = isbn;
 		this.estado = estado;
-		this.descripcion = descripcion;
+		this.desc = desc;
+	}
+	public void SetTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public void SetAutor(String autor) {
+		this.autor = autor;
+	}
+	public void SetFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public void SetPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+	public void SetEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	public void SetGenero(String genero) {
+		this.genero = genero;
+	}
+	public void SetIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public void SetUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public void SetEstado(String estado) {
+		this.estado = estado;
+	}
+	public void SetDesc(String desc) {
+		this.desc = desc;
+	}
+	public String GetTitulo() {
+		return titulo;
+	}
+	public String GetAutor() {
+		return autor;
+	}
+	public String GetFecha() {
+		return fecha;
+	}
+	public int GetPaginas() {
+		return paginas;
+	}
+	public String GetEditorial() {
+		return editorial;
+	}
+	public String GetGenero() {
+		return genero;
+	}
+	public String GetIsbn() {
+		return isbn;
+	}
+	public String GetUbicacion() {
+		return ubicacion;
+	}
+	public String GetEstado() {
+		return estado;
+	}
+	public String GetDesc() {
+		return desc;
 	}
 }
--- Proyecto 3/Biblioteca/src/Bib/Main.java
+++ Proyecto 3/Biblioteca/src/Bib/Main.java
@@ -35,28 +35,39 @@ public class Main {
 				String isbn;
 				String ubicacion;
 				String estado;
-				String descripcion;
+				String desc;
 				System.out.println("Indique el titulo:  \n");
 				Scanner Ititulo = new Scanner(System.in);
 				titulo = Ititulo.nextLine();
 				System.out.println("Indique el o los autores:  \n");
 				Scanner Iautor = new Scanner(System.in);
 				autor = Iautor.nextLine();
-				System.out.println("Indique la fecha (formato DD/MM/AAAA):  \n");
-				Scanner Ititulo = new Scanner(System.in);
-				titulo = Ititulo.nextLine();
-				System.out.println("Indique el titulo:  \n");
-				Scanner Ititulo = new Scanner(System.in);
-				titulo = Ititulo.nextLine();
-				System.out.println("Indique el titulo:  \n");
-				Scanner Ititulo = new Scanner(System.in);
-				titulo = Ititulo.nextLine();
-				System.out.println("Indique el titulo:  \n");
-				Scanner Ititulo = new Scanner(System.in);
-				titulo = Ititulo.nextLine();
-				System.out.println("Indique el titulo:  \n");
-				Scanner Ititulo = new Scanner(System.in);
-				titulo = Ititulo.nextLine();
+				System.out.println("Indique la fecha (formato DD/MM/AAAA):  \n"); //verificar formato
+				Scanner Ifecha = new Scanner(System.in);
+				fecha = Ifecha.nextLine();
+				System.out.println("Indique el numero de paginas:  \n"); //verificar que sea un numero
+				Scanner Ipaginas = new Scanner(System.in);
+				paginas = Ipaginas.nextInt();
+				System.out.println("Indique la editorial:  \n");
+				Scanner Ieditorial = new Scanner(System.in);
+				editorial = Ieditorial.nextLine();
+				System.out.println("Indique el genero:  \n");
+				Scanner Igenero = new Scanner(System.in);
+				genero = Igenero.nextLine();
+				System.out.println("Indique el numero ISBN:  \n");
+				Scanner Iisbn = new Scanner(System.in);
+				isbn = Iisbn.nextLine();
+				System.out.println("Indique la ubicacion:  \n");
+				Scanner Iubicacion = new Scanner(System.in);
+				ubicacion = Iubicacion.nextLine();
+				System.out.println("Indique el estado del libro:  \n");
+				Scanner Iestado = new Scanner(System.in);
+				estado = Iestado.nextLine();
+				System.out.println("Indique la desc:  \n");
+				Scanner Idesc = new Scanner(System.in);
+				desc = Idesc.nextLine();
+				Libro libro = new Libro(titulo, autor, fecha, paginas, editorial, genero, isbn, ubicacion, estado, desc);
+				LibrosList.add(libro);
 			}
 		}
 	}
